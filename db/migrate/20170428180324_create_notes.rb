0033858f2f5e926f939da26707e4d2fbcb6753ba
angmas <angela.mashlan@safeco.com>
2017-04-28 14:43:42 -0400

    Updated note migration file
    -changed note_detail from string to text datatype (text can store more)
    -made note_title not null
    -made timestamp not null
    -made user_id not null, and an index

--- db/migrate/20170428180324_create_notes.rb
+++ db/migrate/20170428180324_create_notes.rb
@@ -1,12 +1,12 @@
 class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
-      t.string :note_title
-      t.string :note_detail
+      t.string :note_title, null: false
+      t.text :note_detail
       t.boolean :favorite
-      t.references :user, foreign_key: true
+      t.references :user, index: true, foreign_key: true, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
